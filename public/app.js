c10d404fdb13b9587c3134024d0ec0e2d7cf6dbb
Florian Fries <fries.florian@gmail.com>
2015-04-03 20:10:07 +0200

    First attempt on the mailchimp subscription

--- public/app.js
+++ public/app.js
@@ -3,6 +3,7 @@
 angular.module('LaserPong', [
     'ui.router',
     'facebook',
+    'mailchimp',
     'ngWebsocket'
 ])
 .config(['$stateProvider', '$urlRouterProvider', 'FacebookProvider',
@@ -106,7 +107,64 @@ function($rootScope, $scope, $timeout, Facebook, $http, $state) {
     };
 }])
 .controller('subscribeCtrl', ['$rootScope', '$scope', function($rootScope, $scope) {
-    //$scope
+    $scope.sendMailchimp = function() {
+        var actions,
+            MailChimpSubscription,
+            params,
+            url;
+
+        // Create a resource for interacting with the MailChimp API
+        url = '//' + mailchimp.username + '.' + mailchimp.dc + '.list-manage.com/subscribe/post-json';
+        params = {
+          'EMAIL': mailchimp.email,
+          'FNAME': mailchimp.fname,
+          'LNAME': mailchimp.lname,
+          'c': 'JSON_CALLBACK',
+          'u': mailchimp.u,
+          'id': mailchimp.id
+        };
+        actions = {
+          'save': {
+            method: 'jsonp'
+          }
+        };
+        MailChimpSubscription = $resource(url, params, actions);
+
+        // Send subscriber data to MailChimp
+        MailChimpSubscription.save(
+          // Successfully sent data to MailChimp.
+          function (response) {
+            // Define message containers.
+            mailchimp.successMessage = '';
+            mailchimp.errorMessage = '';
+
+            // Store the result from MailChimp
+            mailchimp.result = response.result;
+
+            // Mailchimp returned an error.
+            if (response.result === 'error') {
+              if (response.msg) {
+                // Remove error numbers, if any.
+                var errorMessageParts = response.msg.split(' - ');
+                if (errorMessageParts.length > 1)
+                  errorMessageParts.shift(); // Remove the error number
+                mailchimp.errorMessage = errorMessageParts.join(' ');
+              } else {
+                mailchimp.errorMessage = 'Sorry! An unknown error occured.';
+              }
+            }
+            // MailChimp returns a success.
+            else if (response.result === 'success') {
+              mailchimp.successMessage = "Thanks for subscribing! Please confirm the email you receive to complete the process.";
+            }
+          },
+
+          // Error sending data to MailChimp
+          function (error) {
+            $log.error('MailChimp Error: %o', error);
+          }
+        );
+    }
 }])
 .controller('gameCtrl', ['$rootScope', '$scope', function($rootScope, $scope) {
     $scope.up = function() {
@@ -160,7 +218,7 @@ function($rootScope, $scope, $timeout, Facebook, $http, $state) {
 
     // States
     $rootScope.$on('$stateChangeStart', function(e, toState, toParams, fromState, fromParams) {
-        if(!$rootScope.user && toState.name != 'start') {
+        if(!$rootScope.user && toState.name != 'start' && toState.name != 'subscribe') {
             e.preventDefault();
             $state.go('start');
         }
